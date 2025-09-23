6e14afb25922f22d68dcb6e00a0868f9b1e2b164
Janhvi <davjanhvi9d17@gmail.com>
2025-09-23 10:57:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,10 @@ import Leaderboard from "./pages/Leaderboard";
 import PublicView from "./pages/PublicView";
 import Transparency from "./pages/Transparency";
 import NGOPartners from "./pages/NGOPartners";
-
 import Profile from './pages/Profile';
+import ManageIssues from './pages/ManageIssues';
+import Heatmap from './pages/Heatmap';
+import Chatbot from './components/Chatbot';
 
 const queryClient = new QueryClient();
 
@@ -41,10 +43,16 @@ const App = () => (
               <Route path="/transparency" element={<Transparency />} />
               <Route path="/ngo" element={<NGOPartners />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/manage-issues" element={<ManageIssues />} />
+              <Route path="/heatmap" element={<Heatmap />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
+          
+          {/* Global Chatbot */}
+          <Chatbot />
+          
         </TooltipProvider>
       </AppProvider>
     </ThemeProvider>
